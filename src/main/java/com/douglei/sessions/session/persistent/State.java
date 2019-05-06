48ca5ea1ef91bfa9c2423e57841e9d9bb9b3c597
DougLei <953034206@qq.com>
2019-05-06 22:50:52 +0800

    t

--- src/main/java/com/douglei/sessions/session/persistent/State.java
+++ src/main/java/com/douglei/sessions/session/persistent/State.java
@@ -7,11 +7,12 @@ package com.douglei.sessions.session.persistent;
 public enum State {
 	/**
 	 * 新实例状态
-	 * 即添加数据时的状态
+	 * 即添加新数据时的状态
 	 */
 	NEW_INSTANCE,
 	/**
 	 * 持久状态
+	 * 即和数据库有关联时的状态
 	 */
 	PERSISTENT;
 }
