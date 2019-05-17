ed329926176e4fbbcceee35e56bb8dead2fe946e
DougLei <953034206@qq.com>
2019-05-17 12:09:33 +0800

    t

--- src/main/java/com/douglei/sessions/session/sql/impl/SQLSessionImpl.java
+++ src/main/java/com/douglei/sessions/session/sql/impl/SQLSessionImpl.java
@@ -1,8 +1,11 @@
 package com.douglei.sessions.session.sql.impl;
 
+import com.douglei.configuration.environment.mapping.Mapping;
+import com.douglei.configuration.environment.mapping.MappingType;
 import com.douglei.configuration.environment.mapping.MappingWrapper;
 import com.douglei.configuration.environment.property.EnvironmentProperty;
 import com.douglei.database.sql.ConnectionWrapper;
+import com.douglei.sessions.session.MappingMismatchingException;
 import com.douglei.sessions.session.sql.SQLSession;
 import com.douglei.sessions.sqlsession.impl.SqlSessionImpl;
 
@@ -15,5 +18,16 @@ public class SQLSessionImpl extends SqlSessionImpl implements SQLSession {
 	
 	public SQLSessionImpl(ConnectionWrapper connection, EnvironmentProperty environmentProperty, MappingWrapper mappingWrapper) {
 		super(connection, environmentProperty, mappingWrapper);
+	}
+	
+	private Mapping getMapping(String code) {
+		Mapping mapping = mappingWrapper.getMapping(code);
+		if(mapping == null) {
+			throw new NullPointerException("不存在code为["+code+"]的映射");
+		}
+		if(mapping.getMappingType() != MappingType.SQL) {
+			throw new MappingMismatchingException("传入code=["+code+"], 获取的mapping不是["+MappingType.SQL+"]类型");
+		}
+		return mapping;
 	}
 }
