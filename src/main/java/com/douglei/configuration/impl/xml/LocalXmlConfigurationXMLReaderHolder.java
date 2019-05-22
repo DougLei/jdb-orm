02616cfa2233624df8cd79483bd17add881dcf02
DougLei <953034206@qq.com>
2019-05-22 14:40:52 +0800

    t

--- src/main/java/com/douglei/configuration/impl/xml/LocalXmlConfigurationXMLReaderHolder.java
+++ src/main/java/com/douglei/configuration/impl/xml/LocalXmlConfigurationXMLReaderHolder.java
@@ -71,7 +71,7 @@ public class LocalXmlConfigurationXMLReaderHolder {
 class XmlReader{
 	SAXReader configurationSAXReader;// 读取xml configuration文件的SAXReader实例
 	SAXReader tableMappingSAXReader;// 读取table-xml映射文件的SAXReader实例
-	DocumentBuilder sqlMappingDocumentBuilder;// 读取sql-xml映射文件的DocumentBuilder实例
 	
+	DocumentBuilder sqlMappingDocumentBuilder;// 读取sql-xml映射文件的DocumentBuilder实例
 	XPathExpression contentNodeXPathExpression;// 读取sql-xml时, 获取<content>节点的表达式
 }
