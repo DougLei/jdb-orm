3ee309643ca99170f278e5a781a57f8cc9a596d0
DougLei <953034206@qq.com>
2019-06-03 09:59:58 +0800

    t

--- src/main/java/com/douglei/core/dialect/impl/sqlserver/datatype/handler/classtype/ClobDataTypeHandler.java
+++ src/main/java/com/douglei/core/dialect/impl/sqlserver/datatype/handler/classtype/ClobDataTypeHandler.java
@@ -1,20 +1,28 @@
 package com.douglei.core.dialect.impl.sqlserver.datatype.handler.classtype;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.douglei.core.dialect.datatype.DBDataType;
-import com.douglei.core.dialect.datatype.handler.classtype.AbstractStringDataTypeHandler;
+import com.douglei.core.dialect.datatype.handler.classtype.AbstractClobDataTypeHandler;
 import com.douglei.core.dialect.impl.sqlserver.datatype.Varcharmax;
 
 /**
  * 
  * @author DougLei
  */
-public class ClobDataTypeHandler extends AbstractStringDataTypeHandler{
+public class ClobDataTypeHandler extends AbstractClobDataTypeHandler{
 	private ClobDataTypeHandler() {}
 	private static final ClobDataTypeHandler instance = new ClobDataTypeHandler();
 	public static final ClobDataTypeHandler singleInstance() {
 		return instance;
 	}
-
+	
+	@Override
+	public void setValue(PreparedStatement preparedStatement, short parameterIndex, Object value) throws SQLException {
+		StringDataTypeHandler.singleInstance().setValue(preparedStatement, parameterIndex, value);
+	}
+	
 	@Override
 	public DBDataType defaultDBDataType() {
 		return Varcharmax.singleInstance();
