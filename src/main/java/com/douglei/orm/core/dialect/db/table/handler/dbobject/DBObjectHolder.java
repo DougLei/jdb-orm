dade1e541592ae8badc4696ed3858ca3b87305a7
DougLei <953034206@qq.com>
2019-06-25 17:57:33 +0800

    t

--- src/main/java/com/douglei/orm/core/dialect/db/table/handler/dbobject/DBObjectHolder.java
+++ src/main/java/com/douglei/orm/core/dialect/db/table/handler/dbobject/DBObjectHolder.java
@@ -6,6 +6,11 @@ package com.douglei.orm.core.dialect.db.table.handler.dbobject;
  * @author DougLei
  */
 public class DBObjectHolder {
+	/*
+	 * originObject用来记录对表、约束、索引直接操作时的对象
+	 * targetObject和originObject搭配使用, 当修改表名、列名、列数据类型等时, 用来记录目标数据对象和原数据对象
+	 */
+	
 	private Object originObject;// 原数据对象
 	private Object targetObject;// 目标数据对象
 	private DBObjectType dbObjectType;
