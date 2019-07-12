fc3a06934ce3c9e5f91f2f6318920e787d6dc836
DougLei <953034206@qq.com>
2019-07-12 15:39:37 +0800

    t

--- src/main/java/com/douglei/orm/core/dialect/impl/mysql/db/object/DBObjectHandlerImpl.java
+++ src/main/java/com/douglei/orm/core/dialect/impl/mysql/db/object/DBObjectHandlerImpl.java
@@ -1,6 +1,9 @@
 package com.douglei.orm.core.dialect.impl.mysql.db.object;
 
+import java.util.List;
+
 import com.douglei.orm.core.dialect.db.object.DBObjectHandler;
+import com.douglei.orm.core.dialect.db.object.DBObjectType;
 
 /**
  * 
@@ -11,5 +14,14 @@ public class DBObjectHandlerImpl extends DBObjectHandler {
 	@Override
 	protected short nameMaxLength() {
 		return 64;
+	}
+
+	@Override
+	public String getQueryDBObjectIsExistsSqlStatement(DBObjectType dbObjectType, String dbObjectName, List<Object> parameters) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		return null;
 	}
 }
