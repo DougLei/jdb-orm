0cbd7f6d1dbb4e5548458735e2a140a4558901eb
DougLei <953034206@qq.com>
2019-09-10 10:14:45 +0800

    t

--- src/main/java/com/douglei/orm/configuration/impl/xml/element/properties/Properties.java
+++ src/main/java/com/douglei/orm/configuration/impl/xml/element/properties/Properties.java
@@ -91,7 +91,7 @@ public class Properties implements SelfProcessing{
 	 */
 	private String decodeValue(Object value, boolean decodeValue) {
 		if(decodeValue) {
-			return CryptographyUtil.decode(value.toString());
+			return CryptographyUtil.decodeByBASE64(value.toString());
 		}
 		return value.toString();
 	}
