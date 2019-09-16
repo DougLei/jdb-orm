3feacf17cb9f4082617c7f51007aa52c4d49783b
DougLei <953034206@qq.com>
2019-09-16 10:44:45 +0800

    t

--- src/main/java/com/douglei/orm/core/metadata/validator/ValidationResult.java
+++ src/main/java/com/douglei/orm/core/metadata/validator/ValidationResult.java
@@ -11,6 +11,8 @@ public abstract class ValidationResult extends Result{
 	private short index; // 如果验证的是集合, 该字段用来记录验证数据在集合中的下标
 	private String validateFieldName;
 	
+	public ValidationResult() {
+	}
 	public ValidationResult(String validateFieldName) {
 		this.validateFieldName = validateFieldName;
 	}
@@ -18,7 +20,7 @@ public abstract class ValidationResult extends Result{
 	public final void setIndex(short index) {
 		this.index = index;
 	}
-	public final String getValidateFieldName() {
+	public String getValidateFieldName() {
 		return validateFieldName;
 	}
 	public final short getIndex() {
