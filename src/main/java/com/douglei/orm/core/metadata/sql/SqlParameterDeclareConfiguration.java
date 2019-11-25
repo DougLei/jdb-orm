4320ce54ed982ff5e895d907ed34f24a00d2010c
DougLei <953034206@qq.com>
2019-11-25 12:01:18 +0800

    t

--- src/main/java/com/douglei/orm/core/metadata/sql/SqlParameterDeclareConfiguration.java
+++ src/main/java/com/douglei/orm/core/metadata/sql/SqlParameterDeclareConfiguration.java
@@ -3,14 +3,13 @@ package com.douglei.orm.core.metadata.sql;
 import java.util.regex.Pattern;
 
 import com.douglei.orm.configuration.JdbConfigurationBean;
+import com.douglei.tools.utils.StringUtil;
 
 /**
  * sql参数声明的配置类
  * @author DougLei
  */
 public class SqlParameterDeclareConfiguration {
-	private static final char[] pattern_keys = {'$', '(', ')', '*', '+', '.', '[', '?', '\\', '^', '{', '|'};
-	
 	public static final String prefix;
 	public static final String suffix;
 	public static final byte prefixLength;
@@ -28,36 +27,16 @@ public class SqlParameterDeclareConfiguration {
 		suffix = JdbConfigurationBean.instance().getSqlParameterSuffix();
 		prefixLength = (byte) prefix.length();
 		
-		prefixPatternString = toPatternString(prefix);
+		prefixPatternString = StringUtil.toPatternString(prefix);
 		prefixPattern = Pattern.compile(prefixPatternString, Pattern.MULTILINE);
 		if(prefix.equals(suffix)) {
 			suffixPatternString = prefixPatternString;
 			suffixPattern = prefixPattern;
 		}else {
-			suffixPatternString = toPatternString(suffix);
+			suffixPatternString = StringUtil.toPatternString(suffix);
 			suffixPattern = Pattern.compile(suffixPatternString, Pattern.MULTILINE);
 		}
 		
-		sqlParameterSplit = toPatternString(JdbConfigurationBean.instance().getSqlParameterSplit());
-	}
-	
-	// 获取正则表达式, 如果有正则表达式的关键字, 则追加\进行转义
-	private static String toPatternString(String pattern) {
-		StringBuilder sp = new StringBuilder(pattern.length()*2);
-		char s;
-		for(byte i=0;i<pattern.length();i++) {
-			s = pattern.charAt(i);
-			for(char c : pattern_keys) {
-				if(s == c) {
-					sp.append('\\');
-					break;
-				}
-			}
-			sp.append(s);
-		}
-		if(sp.length() == pattern.length()) {
-			return pattern;
-		}
-		return sp.toString();
+		sqlParameterSplit = StringUtil.toPatternString(JdbConfigurationBean.instance().getSqlParameterSplit());
 	}
 }
