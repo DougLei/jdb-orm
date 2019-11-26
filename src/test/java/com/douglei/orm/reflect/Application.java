2b78d25f60a3c0487a682d19f159e9db79639848
DougLei <953034206@qq.com>
2019-11-26 10:37:19 +0800

    t

--- src/test/java/com/douglei/orm/reflect/Application.java
+++ src/test/java/com/douglei/orm/reflect/Application.java
@@ -1,12 +1,19 @@
 package com.douglei.orm.reflect;
 
+import java.io.File;
+
+import com.douglei.orm.core.metadata.table.TableMetadata;
+import com.douglei.tools.utils.serialize.JdkSerializeProcessor;
+
 public class Application {
 	public static void main(String[] args) throws Exception {
-		Class<?> clz = Class.forName("com.douglei.test.reflect.Entity");
+//		Class<?> clz = Class.forName("com.douglei.test.reflect.Entity");
+//		System.out.println(clz.getMethod("singleInstance").invoke(null));
+//		System.out.println(clz.getMethod("singleInstance").invoke(null));
+//		System.out.println(clz.getMethod("singleInstance").invoke(null));
+//		System.out.println(clz.getMethod("singleInstance").invoke(null));
 		
-		System.out.println(clz.getMethod("singleInstance").invoke(null));
-		System.out.println(clz.getMethod("singleInstance").invoke(null));
-		System.out.println(clz.getMethod("singleInstance").invoke(null));
-		System.out.println(clz.getMethod("singleInstance").invoke(null));
+		TableMetadata table = JdkSerializeProcessor.deserializeFromFile(TableMetadata.class, new File("C:\\Users\\Administrator.USER-20190410XF\\Desktop\\SMT_BASE_CFG_PROPERTY_ARR.orm"));
+		System.out.println(table);
 	}
 }
