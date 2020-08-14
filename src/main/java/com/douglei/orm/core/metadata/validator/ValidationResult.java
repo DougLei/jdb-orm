3b56108114c55f6f56fad15df12046a37633ec4e
DougLei <953034206@qq.com>
2020-08-14 14:34:53 +0800

    t

--- src/main/java/com/douglei/orm/core/metadata/validator/ValidationResult.java
+++ src/main/java/com/douglei/orm/core/metadata/validator/ValidationResult.java
@@ -1,12 +1,12 @@
 package com.douglei.orm.core.metadata.validator;
 
-import com.douglei.tools.clz.I18NResult;
+import com.douglei.i18n.Result;
 
 /**
  * 验证结果对象
  * @author DougLei
  */
-public abstract class ValidationResult extends I18NResult{
+public abstract class ValidationResult extends Result{
 	public static final String i18nCodePrefix = "jdb.data.validator.";
 	private short index; // 如果验证的是集合, 该字段用来记录验证数据在集合中的下标
 	private String validateFieldName;
