4b9a70d6eca90d0ee5ea5a6f7c0cbaaf5b667a7f
DougLei <953034206@qq.com>
2020-10-20 16:37:15 +0800

    t

--- src/main/java/com/douglei/orm/mapping/impl/table/TableMappingParser.java
+++ src/main/java/com/douglei/orm/mapping/impl/table/TableMappingParser.java
@@ -397,7 +397,7 @@ class TableMappingParser extends MappingParser<TableMapping>{
 	private MappingProperty getTableMappingProperty(Element rootElement) {
 		MappingProperty property = new MappingProperty(tableMetadata.getCode(), MappingTypeConstants.TABLE);
 		Element propertyElement = rootElement.element("property");
-		if(property != null)
+		if(propertyElement != null)
 			property.setValues(propertyElement.attributeValue("supportCover"), propertyElement.attributeValue("supportDelete"), propertyElement.attributeValue("extendExpr"));
 		return property;
 	}
