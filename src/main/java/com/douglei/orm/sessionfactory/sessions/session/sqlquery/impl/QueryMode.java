99c5b4f8f462c0446da0a7c01270c01795867a0a
DougLei <953034206@qq.com>
2021-03-18 16:24:16 +0800

    t

--- src/main/java/com/douglei/orm/sessionfactory/sessions/session/sqlquery/impl/QueryMode.java
+++
@@ -1,9 +0,0 @@
-package com.douglei.orm.sessionfactory.sessions.session.sqlquery.impl;
-
-/**
- * 
- * @author DougLei
- */
-public enum QueryMode {
-	QUERY, UNIQUE_QUERY, LIMIT_QUERY, COUNT_QUERY, PAGE_QUERY, RECURSIVE_QUERY, PAGE_RECURSIVE_QUERY
-}
